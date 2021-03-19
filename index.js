6e44cd269d47eb5468c84f1a5003ca91a6babb55
Jerelynn Moran <jerelynn01@icloud.com>
2021-03-19 05:21:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Neil deGrasse Tyson"
+var specialGuest = "Hello, Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = " space.";
+var topic = " space..";
 
 var conversation = "Let\'s talk about";
